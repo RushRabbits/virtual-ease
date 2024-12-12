1121b9ec7620c8d2a1717d49a9a3a3711a460de4
突突兔 <1473601995@qq.com>
2024-12-12 19:28:59 +0800

    feat: web入口模块

---
+++ virtual-ease-admin/src/main/java/com/awake/ve/admin/VirtualEaseApplication.java
@@ -0,0 +1,11 @@
+package com.awake.ve.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualEaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualEaseApplication.class, args);
+    }
+}

94a176468cb80794472442f95f93484a4b17e258
突突兔 <1473601995@qq.com>
2024-12-13 09:34:56 +0800

    feat: ve-common-core模块搭建

--- ve-admin/src/main/java/com/awake/ve/admin/VirtualEaseApplication.java
+++ ve-admin/src/main/java/com/awake/ve/admin/VirtualEaseApplication.java
@@ -2,7 +2,9 @@ package com.awake.ve.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "com.awake.ve")
 @SpringBootApplication
 public class VirtualEaseApplication {
     public static void main(String[] args) {

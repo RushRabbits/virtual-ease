5f4542bc39c37c65c7f56bd3ad2a6c7a388e16fb
突突兔 <1473601995@qq.com>
2024-12-31 11:19:10 +0800

    fix: 移除代码生成器模块

--- ve-admin/src/main/java/com/awake/ve/admin/VirtualEaseApplication.java
+++ ve-admin/src/main/java/com/awake/ve/admin/VirtualEaseApplication.java
@@ -2,10 +2,8 @@ package com.awake.ve.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = "com.awake.ve")
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.awake.ve")
 public class VirtualEaseApplication {
     public static void main(String[] args) {
         SpringApplication.run(VirtualEaseApplication.class, args);

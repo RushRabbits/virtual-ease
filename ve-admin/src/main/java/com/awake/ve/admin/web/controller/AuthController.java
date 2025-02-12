673841eebe601a0829336996a8517abb7b6805c2
突突兔 <1473601995@qq.com>
2025-02-12 15:51:36 +0800

    feat: AuthController

--- ve-admin/src/main/java/com/awake/ve/admin/web/controller/AuthController.java
+++ ve-admin/src/main/java/com/awake/ve/admin/web/controller/AuthController.java
@@ -3,22 +3,13 @@ package com.awake.ve.admin.web.controller;
 import cn.dev33.satoken.annotation.SaIgnore;
 import cn.dev33.satoken.exception.NotLoginException;
 import cn.hutool.core.codec.Base64;
-import cn.hutool.core.collection.CollUtil;
-import cn.hutool.core.util.ObjectUtil;
 import com.awake.ve.admin.web.domain.vo.LoginTenantVo;
 import com.awake.ve.admin.web.domain.vo.LoginVo;
 import com.awake.ve.admin.web.domain.vo.TenantListVo;
 import com.awake.ve.admin.web.service.SysLoginService;
 import com.awake.ve.admin.web.service.SysRegisterService;
 import com.awake.ve.admin.web.service.auth.IAuthStrategy;
-import com.awake.ve.common.core.constant.UserConstants;
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import me.zhyd.oauth.model.AuthResponse;
-import me.zhyd.oauth.model.AuthUser;
-import me.zhyd.oauth.request.AuthRequest;
-import me.zhyd.oauth.utils.AuthStateUtils;
+import com.awake.ve.common.core.constant.SystemConstants;
 import com.awake.ve.common.core.domain.R;
 import com.awake.ve.common.core.domain.model.LoginBody;
 import com.awake.ve.common.core.domain.model.RegisterBody;
@@ -38,6 +29,14 @@ import com.awake.ve.system.service.ISysClientService;
 import com.awake.ve.system.service.ISysConfigService;
 import com.awake.ve.system.service.ISysSocialService;
 import com.awake.ve.system.service.ISysTenantService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.zhyd.oauth.model.AuthResponse;
+import me.zhyd.oauth.model.AuthUser;
+import me.zhyd.oauth.request.AuthRequest;
+import me.zhyd.oauth.utils.AuthStateUtils;
+import org.springframework.util.CollectionUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -48,11 +47,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ScheduledExecutorService;
 
-/**
- * 认证
- *
- * @author Lion Li
- */
 @Slf4j
 @SaIgnore
 @RequiredArgsConstructor
@@ -65,16 +59,16 @@ public class AuthController {
     private final SysRegisterService registerService;
     private final ISysConfigService configService;
     private final ISysTenantService tenantService;
-    private final ISysSocialService socialUserService;
+    private final ISysSocialService socialService;
     private final ISysClientService clientService;
     private final ScheduledExecutorService scheduledExecutorService;
 
-
     /**
-     * 登录方法
+     * 登录
      *
-     * @param body 登录信息
-     * @return 结果
+     * @param body 登录请求体
+     * @author wangjiaxing
+     * @date 2025/2/12 15:14
      */
     @ApiEncrypt
     @PostMapping("/login")
@@ -85,86 +79,103 @@ public class AuthController {
         String clientId = loginBody.getClientId();
         String grantType = loginBody.getGrantType();
         SysClientVo client = clientService.queryByClientId(clientId);
-        // 查询不到 client 或 client 内不包含 grantType
-        if (ObjectUtil.isNull(client) || !StringUtils.contains(client.getGrantType(), grantType)) {
-            log.info("客户端id: {} 认证类型：{} 异常!.", clientId, grantType);
+
+        // 查询不到client或者client内不包含grantType
+        if (client == null || !StringUtils.contains(client.getGrantType(), grantType)) {
+            log.info("[AuthController][login] 客户端id:{} ,认证类型:{} 异常", clientId, grantType);
             return R.fail(MessageUtils.message("auth.grant.type.error"));
-        } else if (!UserConstants.NORMAL.equals(client.getStatus())) {
+        } else if (!SystemConstants.NORMAL.equals(client.getStatus())) {
             return R.fail(MessageUtils.message("auth.grant.type.blocked"));
         }
+
         // 校验租户
         loginService.checkTenant(loginBody.getTenantId());
+
         // 登录
         LoginVo loginVo = IAuthStrategy.login(body, client, grantType);
 
-/*        Long userId = LoginHelper.getUserId();
-        scheduledExecutorService.schedule(() -> {
-            SseMessageDto dto = new SseMessageDto();
-            dto.setMessage("欢迎登录");
-            dto.setUserIds(List.of(userId));
-            SseMessageUtils.publishMessage(dto);
-        }, 5, TimeUnit.SECONDS);*/
+        // Long userId = LoginHelper.getUserId();
+        // scheduledExecutorService.schedule(() -> {
+        //     SseMessageDto dto = new SseMessageDto();
+        //     dto.setMessage("欢迎登录");
+        //     dto.setUserIds(List.of(userId));
+        //     SseMessageUtils.publishMessage(dto);
+        // }, 5, TimeUnit.SECONDS);
         return R.ok(loginVo);
     }
 
     /**
      * 第三方登录请求
      *
-     * @param source 登录来源
-     * @return 结果
+     * @param source   第三方平台
+     * @param tenantId 租户id
+     * @param domain   域名
      */
     @GetMapping("/binding/{source}")
     public R<String> authBinding(@PathVariable("source") String source,
-                                 @RequestParam String tenantId, @RequestParam String domain) {
-        SocialLoginConfigProperties obj = socialProperties.getType().get(source);
-        if (ObjectUtil.isNull(obj)) {
+                                 @RequestParam String tenantId,
+                                 @RequestParam String domain) {
+        SocialLoginConfigProperties socialLoginConfigProperties = socialProperties.getType().get(source);
+        if (socialLoginConfigProperties == null) {
             return R.fail(source + "平台账号暂不支持");
         }
+
         AuthRequest authRequest = SocialUtils.getAuthRequest(source, socialProperties);
         Map<String, String> map = new HashMap<>();
         map.put("tenantId", tenantId);
         map.put("domain", domain);
         map.put("state", AuthStateUtils.createState());
         String authorizeUrl = authRequest.authorize(Base64.encode(JsonUtils.toJsonString(map), StandardCharsets.UTF_8));
-        return R.ok("操作成功", authorizeUrl);
+        return R.ok(authorizeUrl);
     }
 
     /**
-     * 第三方登录回调业务处理 绑定授权
+     * 第三方登录回调 业务处理 绑定授权
      *
-     * @param loginBody 请求体
-     * @return 结果
+     * @param loginBody {@link SocialLoginBody}
+     * @author wangjiaxing
+     * @date 2025/2/12 15:29
      */
     @PostMapping("/social/callback")
     public R<Void> socialCallback(@RequestBody SocialLoginBody loginBody) {
         // 获取第三方登录信息
         AuthResponse<AuthUser> response = SocialUtils.loginAuth(
-            loginBody.getSource(), loginBody.getSocialCode(),
-            loginBody.getSocialState(), socialProperties);
+                loginBody.getSource(),
+                loginBody.getSocialCode(),
+                loginBody.getSocialState(),
+                socialProperties
+        );
+
         AuthUser authUserData = response.getData();
+
         // 判断授权响应是否成功
         if (!response.ok()) {
             return R.fail(response.getMsg());
         }
+
         loginService.socialRegister(authUserData);
+
         return R.ok();
     }
-
 
     /**
      * 取消授权
      *
-     * @param socialId socialId
+     * @param socialId 第三方id
+     * @author wangjiaxing
+     * @date 2025/2/12 15:34
      */
-    @DeleteMapping(value = "/unlock/{socialId}")
+    @DeleteMapping("/unlock/{socialId}")
     public R<Void> unlockSocial(@PathVariable Long socialId) {
-        Boolean rows = socialUserService.deleteWithValidById(socialId);
+        Boolean rows = socialService.deleteWithValidById(socialId);
         return rows ? R.ok() : R.fail("取消授权失败");
     }
 
-
     /**
      * 退出登录
+     *
+     * @author wangjiaxing
+     * @date 2025/2/12 15:35
      */
     @PostMapping("/logout")
     public R<Void> logout() {
@@ -174,37 +185,43 @@ public class AuthController {
 
     /**
      * 用户注册
+     *
+     * @param user {@link RegisterBody}
+     * @author wangjiaxing
+     * @date 2025/2/12 15:37
      */
     @ApiEncrypt
     @PostMapping("/register")
     public R<Void> register(@Validated @RequestBody RegisterBody user) {
         if (!configService.selectRegisterEnabled(user.getTenantId())) {
-            return R.fail("当前系统没有开启注册功能！");
+            return R.fail("当前系统未开启注册功能");
         }
         registerService.register(user);
-        return R.ok();
+        return R.ok("注册成功");
     }
 
     /**
-     * 登录页面租户下拉框
+     * 登陆页面获取租户下拉框
      *
-     * @return 租户列表
+     * @param request {@link HttpServletRequest}
+     * @return {@link R}<{@link LoginTenantVo}></>
+     * @author wangjiaxing
+     * @date 2025/2/12 15:39
      */
     @GetMapping("/tenant/list")
     public R<LoginTenantVo> tenantList(HttpServletRequest request) throws Exception {
-        // 返回对象
         LoginTenantVo result = new LoginTenantVo();
+
+        // 是否开启多租户
         boolean enable = TenantHelper.isEnable();
-        result.setTenantEnabled(enable);
-        // 如果未开启租户这直接返回
         if (!enable) {
             return R.ok(result);
         }
 
         List<SysTenantVo> tenantList = tenantService.queryList(new SysTenantBo());
         List<TenantListVo> voList = MapstructUtils.convert(tenantList, TenantListVo.class);
+
         try {
-            // 如果只超管返回所有租户
             if (LoginHelper.isSuperAdmin()) {
                 result.setVoList(voList);
                 return R.ok(result);
@@ -216,16 +233,15 @@ public class AuthController {
         String host;
         String referer = request.getHeader("referer");
         if (StringUtils.isNotBlank(referer)) {
-            // 这里从referer中取值是为了本地使用hosts添加虚拟域名，方便本地环境调试
+            // 这里从referer取值是为了本地使用hosts添加虚拟域名,方便本地环境调试
             host = referer.split("//")[1].split("/")[0];
         } else {
             host = new URL(request.getRequestURL().toString()).getHost();
         }
+
         // 根据域名进行筛选
-        List<TenantListVo> list = StreamUtils.filter(voList, vo ->
-            StringUtils.equalsIgnoreCase(vo.getDomain(), host));
-        result.setVoList(CollUtil.isNotEmpty(list) ? list : voList);
+        List<TenantListVo> list = StreamUtils.filter(voList, vo -> StringUtils.equalsIgnoreCase(vo.getDomain(), host));
+        result.setVoList(CollectionUtils.isEmpty(list) ? voList : list);
         return R.ok(result);
     }
-
 }

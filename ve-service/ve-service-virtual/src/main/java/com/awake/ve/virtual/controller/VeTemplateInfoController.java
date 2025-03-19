ea696e673d3329fe4f63ebaec633272135ecac4c
突突兔 <1473601995@qq.com>
2025-03-19 15:13:43 +0800

    feat: 根据虚拟机id创建模板

--- ve-service/ve-service-virtual/src/main/java/com/awake/ve/virtual/controller/VeTemplateInfoController.java
+++ ve-service/ve-service-virtual/src/main/java/com/awake/ve/virtual/controller/VeTemplateInfoController.java
@@ -4,9 +4,11 @@ import cn.dev33.satoken.annotation.SaCheckPermission;
 import com.awake.ve.common.core.domain.R;
 import com.awake.ve.virtual.domain.vo.VeTemplateListVo;
 import com.awake.ve.virtual.service.IVeTemplateInfoService;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +32,18 @@ public class VeTemplateInfoController {
     @GetMapping("/list")
     public R<List<VeTemplateListVo>> templates() {
         return R.ok(veTemplateInfoService.templates());
+    }
+
+    /**
+     * 根据虚拟机创建模板
+     *
+     * @param vmId 虚拟机id
+     * @author wangjiaxing
+     * @date 2025/3/19 14:38
+     */
+    @SaCheckPermission("ve:template:createTemplate")
+    @GetMapping("/createTemplate/{vmId}")
+    public R<Boolean> createTemplate(@NotNull(message = "虚拟机不能为空") @PathVariable Long vmId) {
+        return R.ok(veTemplateInfoService.createTemplate(vmId));
     }
 }
--- ve-service/ve-service-virtual/src/main/java/com/awake/ve/virtual/service/impl/VeTemplateInfoServiceImpl.java
+++ ve-service/ve-service-virtual/src/main/java/com/awake/ve/virtual/service/impl/VeTemplateInfoServiceImpl.java
@@ -1,6 +1,9 @@
 package com.awake.ve.virtual.service.impl;
 
 import com.awake.ve.common.core.utils.MapstructUtils;
+import com.awake.ve.common.core.utils.StringUtils;
+import com.awake.ve.common.ecs.api.template.request.PVECreateTemplateApiRequest;
+import com.awake.ve.common.ecs.api.template.response.PVECreateTemplateApiResponse;
 import com.awake.ve.common.ecs.api.vm.status.PVENodeVmListApiRequest;
 import com.awake.ve.common.ecs.api.vm.status.PVENodeVmListApiResponse;
 import com.awake.ve.common.ecs.domain.vm.PveVmInfo;
@@ -40,5 +43,25 @@ public class VeTemplateInfoServiceImpl implements IVeTemplateInfoService {
         vmList = vmList.stream().filter(PveVmInfo::getTemplate).sorted(Comparator.comparing(PveVmInfo::getVmId)).toList();
 
         return MapstructUtils.convert(vmList, VeTemplateListVo.class);
+    }
+
+    /**
+     * 根据虚拟机创建模板
+     *
+     * @param vmId 虚拟机id
+     * @author wangjiaxing
+     * @date 2025/3/19 14:38
+     */
+    @Override
+    public Boolean createTemplate(Long vmId) {
+        ApiHandler apiHandler = PVEApi.CREATE_TEMPLATE.getApiHandler();
+
+        // api参数
+        PVECreateTemplateApiRequest request = PVECreateTemplateApiRequest.builder().node("pve").vmId(vmId).build();
+
+        // api响应
+        PVECreateTemplateApiResponse response = (PVECreateTemplateApiResponse) apiHandler.handle(request);
+
+        return StringUtils.isNotBlank(response.getData());
     }
 }
